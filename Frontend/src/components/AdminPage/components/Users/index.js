f5ccda06ddaf0b7382729f368ba4ffbf68e71dcb
PauloCoelho0 <paulo.coelho.real11@gmail.com>
2023-01-04 22:12:30 +0000

    tabulator (esta a dar erro ao chamar a data)

--- Frontend/src/components/AdminPage/components/Users/index.js
+++ Frontend/src/components/AdminPage/components/Users/index.js
@@ -6,6 +6,9 @@ import styles from "./styles.module.scss";
 import { useGetData } from "../../hooks/useGetData";
 import { UsersContext } from "../../../../contexts/UsersProvider";
 import { usePostData } from "../../hooks/usePostData";
+import { ReactTabulator } from "react-tabulator";
+import "react-tabulator/lib/styles.css"; // default theme
+import "react-tabulator/css/bootstrap/tabulator_bootstrap.min.css"; // use Theme(s)
 
 const Users = () => {
   const { register, handleSubmit } = useForm();
@@ -34,88 +37,150 @@ const Users = () => {
     return <div>UPPSSSS</div>;
   }
 
+  const columns = [
+    // {
+    //   title: "Car ID",
+    //   field: "carId",
+    //   headerFilter: "input",
+    //   formatter: "link",
+    //   formatterParams: {
+    //     url: (cell) => {
+    //       return "http://localhost:3000/caredit/" + cell.getValue();
+    //     },
+    //   },
+    // },
+    {
+      title: "name",
+      field: "name",
+
+      headerFilter: "input",
+    },
+    {
+      title: "email",
+      field: "email",
+
+      headerFilter: "input",
+    },
+    // {
+    //   title: "Idade",
+    //   field: "age",
+
+    //   headerFilter: "input",
+    // },
+    // {
+    //   title: "Morada",
+    //   field: "address",
+
+    //   headerFilter: "input",
+    // },
+    // {
+    //   title: "País de Origem",
+    //   field: "country",
+
+    //   headerFilter: "input",
+    // },
+  ];
+
+  const options = {
+    pagination: "local",
+    paginationSize: 8,
+    movableColumns: true,
+    paginationCounter: "rows",
+    paginationSizeSelector: [3, 6, 8, 10],
+  };
+
+  console.log(data);
+
   return (
-    <Container>
-      <Row>
-        <Col className={styles.column}>
-          <h3>Create User</h3>
-          <div className={styles.container}>
-            <form className={styles.form} onSubmit={handleSubmit(addUser)}>
-              <div className={styles.field}>
-                <label className={styles.label} for="name">
-                  Name:
-                </label>
-                <input
-                  id="name"
-                  type="name"
-                  name="name"
-                  required="required"
-                  {...register("name")}
-                />
-              </div>
-              <div className={styles.field}>
-                <label className={styles.label} for="password">
-                  Password:
-                </label>
-                <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  required="required"
-                  {...register("password")}
-                />
-              </div>
-              <div className={styles.field}>
-                <label className={styles.label} for="email">
-                  Email:
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  required="required"
-                  {...register("email")}
-                />
-              </div>
-              <div className={styles.field}>
-                <label className={styles.label} for="age">
-                  Age :
-                </label>
-                <input id="age" name="age" type="number" {...register("age")} />
-              </div>
-              <div className={styles.field}>
-                <label className={styles.label} for="address">
-                  Address :
-                </label>
-                <input
-                  id="address"
-                  name="address"
-                  required="required"
-                  {...register("address")}
-                />
-              </div>
-              <div className={styles.field}>
-                <label className={styles.label} for="country">
-                  Country :
-                </label>
-                <input
-                  id="country"
-                  name="country"
-                  required="required"
-                  {...register("country")}
-                />
-              </div>
-              <Row>
-                <input className="submit" type="submit" />
-              </Row>
-            </form>
-          </div>
-        </Col>
-        <Col>
-          <Table columns={["name", "email"]} rows={data} />
-        </Col>
-      </Row>
-    </Container>
+    <div className={styles.customTable}>
+      <ReactTabulator
+        columns={columns}
+        layout={"fitColumns"}
+        // data={data}
+        options={options}
+        placeholder={"No Data Set"}
+      />
+    </div>
+
+    // <Container>
+    //   <Row>
+    //     <Col className={styles.column}>
+    //       <h3>Create User</h3>
+    //       <div className={styles.container}>
+    //         <form className={styles.form} onSubmit={handleSubmit(addUser)}>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="name">
+    //               Name:
+    //             </label>
+    //             <input
+    //               id="name"
+    //               type="name"
+    //               name="name"
+    //               required="required"
+    //               {...register("name")}
+    //             />
+    //           </div>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="password">
+    //               Password:
+    //             </label>
+    //             <input
+    //               id="password"
+    //               type="password"
+    //               name="password"
+    //               required="required"
+    //               {...register("password")}
+    //             />
+    //           </div>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="email">
+    //               Email:
+    //             </label>
+    //             <input
+    //               id="email"
+    //               type="email"
+    //               name="email"
+    //               required="required"
+    //               {...register("email")}
+    //             />
+    //           </div>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="age">
+    //               Age :
+    //             </label>
+    //             <input id="age" name="age" type="number" {...register("age")} />
+    //           </div>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="address">
+    //               Address :
+    //             </label>
+    //             <input
+    //               id="address"
+    //               name="address"
+    //               required="required"
+    //               {...register("address")}
+    //             />
+    //           </div>
+    //           <div className={styles.field}>
+    //             <label className={styles.label} for="country">
+    //               Country :
+    //             </label>
+    //             <input
+    //               id="country"
+    //               name="country"
+    //               required="required"
+    //               {...register("country")}
+    //             />
+    //           </div>
+    //           <Row>
+    //             <input className="submit" type="submit" />
+    //           </Row>
+    //         </form>
+    //       </div>
+    //     </Col>
+    //     <Col></Col>
+    //   </Row>
+    // </Container>
   );
 };
 
