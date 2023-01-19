ae6258b160b60499bb2beed35572a83cb911a718
PauloCoelho0 <paulo.coelho.real11@gmail.com>
2023-01-19 01:18:00 +0000

    re-render Dom on Increment Marcacoes

--- Frontend/src/components/UserPage/components/Marcacoes/marcacoes.js
+++ Frontend/src/components/UserPage/components/Marcacoes/marcacoes.js
@@ -5,54 +5,57 @@ import { Card, Button } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const Aula = (props) => {
-  console.log(props.aulaImage);
-  const navigate = useNavigate();
+class Aula extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      participants: props.participants,
+    };
+  }
 
-  const [response, setResponse] = useState(null);
-
-  const handleUpdate = () => {
+  handleUpdate = () => {
     const cookieValue = Cookies.get("userID");
     const valueWithoutJ = cookieValue.substring(3, cookieValue.length - 1);
     const data = { _id: valueWithoutJ };
     axios
-      .put(`http://localhost:3000/aulas/${props._id}`, data)
+      .put(`http://localhost:3000/aulas/${this.props._id}`, data)
       .then((res) => {
         if (res.status === 200) {
-          console.log("Successful Update");
-          setTimeout(function () {
-            window.location.reload(1);
-          }, 500);
+          this.setState((prevState) => {
+            return { participants: prevState.participants + 1 };
+          });
         }
       })
-      .catch((err) => {
-        if (err.response.status === 500) {
-          console.log("User Inscrito QUASE DE CERTEZA.. QUASEEEEEEE");
-        }
-      });
+      .catch((err) => console.log(err));
   };
 
-  return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={props.aulaImage} />
-      <Card.Body>
-        <Card.Title style={{ fontWeight: "700", marginTop: "40px" }}>
-          {props.name}
-        </Card.Title>
-        <Card.Text>
-          <p className="carDetails">Início: {props.beginDate}</p>
-          <p className="carDetails">Fim: {props.endDate}</p>
-          <p className="carDetails">Capacidade: {props.capacity}</p>
-          <p className="carDetails">
-            Incristos: <b>{props.participants}</b>
-          </p>
-        </Card.Text>
-        <Button className="carBtn" variant="secondary" onClick={handleUpdate}>
-          Inscrever
-        </Button>
-      </Card.Body>
-    </Card>
-  );
-};
+  render() {
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={this.props.aulaImage} />
+        <Card.Body>
+          <Card.Title style={{ fontWeight: "700", marginTop: "40px" }}>
+            {this.props.name}
+          </Card.Title>
+          <Card.Text>
+            <p className="carDetails">Início: {this.props.beginDate}</p>
+            <p className="carDetails">Fim: {this.props.endDate}</p>
+            <p className="carDetails">Capacidade: {this.props.capacity}</p>
+            <p className="carDetails">
+              Incristos: <b>{this.state.participants}</b>
+            </p>
+          </Card.Text>
+          <Button
+            className="carBtn"
+            variant="secondary"
+            onClick={this.handleUpdate}
+          >
+            Inscrever
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
 
 export default Aula;
