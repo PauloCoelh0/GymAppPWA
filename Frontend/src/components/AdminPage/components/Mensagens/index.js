8e4ccf94415b4e9081da290ff101ef6faf35143c
PauloCoelho0 <paulo.coelho.real11@gmail.com>
2023-01-22 15:37:54 +0000

    mensagens

--- Frontend/src/components/AdminPage/components/Mensagens/index.js
+++ Frontend/src/components/AdminPage/components/Mensagens/index.js
@@ -7,28 +7,14 @@ const url = "http://localhost:3000/mensagens/create";
 function Mensagens() {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => postMessage(data);
-  const postMessage = async (data) => {
-    const messageData = new FormData();
-    messageData.append("subject", data.subject);
-    messageData.append("text", data.text);
 
-    try {
-      const response = await axios.post(url, messageData, {
-        headers: {},
+  const postMessage = async (data) => {
+    axios
+      .post(url, data)
+      .then((res) => {})
+      .catch((err) => {
+        console.error(err);
       });
-      console.log(response);
-      if (response.status === 200) {
-        alert("Menssagem enviada com SUCESSO!");
-        return response.data.data;
-      } else {
-        alert("ERRO: Mensagem não enviada!");
-      }
-    } catch (error) {
-      console.log("Error creating message: ", error);
-      alert(
-        "ERRO: Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente mais tarde."
-      );
-    }
   };
 
   return (
